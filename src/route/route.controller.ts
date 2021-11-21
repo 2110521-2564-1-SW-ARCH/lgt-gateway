33d08ba9edffd1e39d2252d45aee8ab526c042d4
mintcnn <mintchnrt@gmail.com>
2021-11-21 16:14:13 +0700

    api get field route source, destination

--- src/route/route.controller.ts
+++ src/route/route.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Headers , Param, Post, HttpStatus, HttpCode, Delete, UseGuards, UnauthorizedException } from "@nestjs/common";
+import { Body, Controller, Headers , Param, Post, HttpStatus, HttpCode, Delete, UseGuards, UnauthorizedException, Get } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { GetRoutePayloadDto } from "./dto/get-route.dto";
 import { CreateRoutePayloadDto } from "./dto/create-route.dto";
@@ -11,9 +11,7 @@ import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { JwtUtil } from "src/auth/jwt.util";
 
 
-@ApiBearerAuth()
 @ApiTags('routes')
-@UseGuards(JwtAuthGuard)
 @Controller('api/routes')
 export class RouteController {
     constructor(
@@ -26,23 +24,49 @@ export class RouteController {
     async searchRoute(@Body() RoutePayload: GetRoutePayloadDto): Promise<Observable<AxiosResponse<any>>> {
         const headers = {
             'Content-Type': 'application/json',
-            // 'Authorization': auth,
         }
-        // const tokenDecoded = this.jwtUtil.decode(auth)
-        // if (tokenDecoded.userId){
-            return await this.http
-        .post(
-            `${process.env.ROUTE_URL}/api/routes/search-route`,
-            RoutePayload,
-            {
-                headers: headers
-            })
-        .pipe(map(response => response.data))
-        // } else {
-        //     throw new UnauthorizedException()
-        // }
+        return await this.http
+            .post(
+                `${process.env.ROUTE_URL}/api/routes/search-route`,
+                RoutePayload,
+                {
+                    headers: headers
+                })
+            .pipe(map(response => response.data))
     }
 
+    @HttpCode(HttpStatus.OK)
+    @Get('source')
+    async getSource(): Promise<Observable<AxiosResponse<any>>> {
+        const headers = {
+            'Content-Type': 'application/json',
+        }
+        return await this.http
+            .get(
+                `${process.env.ROUTE_URL}/api/routes/source`,
+                {
+                    headers: headers
+                })
+            .pipe(map(response => response.data))
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Get('destination')
+    async getDestination(): Promise<Observable<AxiosResponse<any>>> {
+        const headers = {
+            'Content-Type': 'application/json',
+        }
+        return await this.http
+            .get(
+                `${process.env.ROUTE_URL}/api/routes/destination`,
+                {
+                    headers: headers
+                })
+            .pipe(map(response => response.data))
+    }
+
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
     @HttpCode(HttpStatus.OK)
     @Post('save-route')
     async createRoute(@Headers('Authorization') auth: string, @Body() RoutePayload: CreateRoutePayloadDto): Promise<Observable<AxiosResponse<any>>> {
@@ -65,6 +89,8 @@ export class RouteController {
         }
     }
 
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
     @HttpCode(HttpStatus.OK)
     @Delete(':id')
     async deleteRoute(@Headers('Authorization') auth: string, @Param('id') routeId: number): Promise<Observable<AxiosResponse<any>>> {
