a2fb65463375bdb0d774bb4ff9af153612d82a37
Jennisa <jennisa200@hotmail.com>
2021-11-24 00:44:23 +0700

    add api property

--- src/travelplan/dto/fulltravelplan.dto.ts
+++ src/travelplan/dto/fulltravelplan.dto.ts
@@ -1,21 +1,27 @@
 import { Prop, Schema } from "@nestjs/mongoose";
 import { IsArray, IsBoolean, IsNotEmpty, IsNumber, IsString } from "class-validator";
-import { ObjectId } from "mongoose";
+
+import { ApiProperty } from "@nestjs/swagger";
 import { ILocation } from "src/location/interfaces/location.interface";
 
 
 export class FullTravelPlanPayloadDto{
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     userName: string
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     planName: string
+    @ApiProperty()
     @IsArray()
     @IsNotEmpty()
     locations: ILocation[]
+    @ApiProperty()
     @IsString()
     description: string
+    @ApiProperty()
     @IsBoolean()
     @IsNotEmpty()
     isPublic: boolean
